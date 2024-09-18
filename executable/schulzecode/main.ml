cbd4d534f52f4d3fa6de8882b556cc608a861b29
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2024-09-18 12:54:30 +0100

    Added the output as well

--- executable/schulzecode/main.ml
+++ executable/schulzecode/main.ml
@@ -54,6 +54,7 @@ let fnmat : coq_Node -> coq_Node -> coq_R =
 let rank (n : coq_Node) : int =
   match n with A -> 0 | B -> 1 | C -> 2 | D -> 3
 
+  (* N Matrix https://link.springer.com/chapter/10.1007/978-3-642-32347-8_7 *)
 let matrix : coq_R array array = 
   [|
     [| oneR; Left 8; Left 14; Left 10|];
@@ -70,3 +71,12 @@ let _ =
   let ret = List.map (fun (x, y) -> (string_candidates x, string_candidates y, string_values (comp x y))) 
     (cross_product finN finN) in 
   print_endline (string_list ret)
+
+  (* 
+  
+  (A, A, Infinity)(A, B, Left 14)(A, C, Left 14)(A, D, Left 12)
+  (B, A, Left 13)(B, B, Infinity)(B, C, Left 13)(B, D, Left 12)
+  (C, A, Left 13)(C, B, Left 15)(C, C, Infinity)(C, D, Left 12)
+  (D, A, Left 13)(D, B, Left 19)(D, C, Left 13)(D, D, Infinity)
+
+  *)
