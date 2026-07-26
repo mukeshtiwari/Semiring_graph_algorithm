fa1e35c0584cfdc2b8998300a92f7620c3e4a316
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2026-07-26 14:18:56 +0100

    Extraction code

---
+++ extraction/fivegslicing/Extraction.v
@@ -0,0 +1,10 @@
+From Stdlib Require Import Extraction.
+From Examples Require Import FiveGNetworkSlicing.
+Set Extraction Output Directory ".".
+
+Extract Inductive nat => int [ "0" "Stdlib.Int.succ" ]
+ "(fun fO fS n -> if n=0 then fO () else fS (n-1))".
+Extract Inductive list => "list" [ "[]" "(::)" ].
+Extract Inductive bool => "bool" [ "true" "false" ].
+
+Separate Extraction FiveGNetworkSlicing.
