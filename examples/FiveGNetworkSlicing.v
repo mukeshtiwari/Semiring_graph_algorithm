5fee8530085276ee7f4200c4fe004bb61aa71e63
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2026-08-04 22:24:32 +0100

    moved to new implementation

--- examples/FiveGNetworkSlicing.v
+++ examples/FiveGNetworkSlicing.v
@@ -1,8 +1,9 @@
 From Stdlib Require Import List BinNatDef
   Psatz Utf8 EqNat.
-From Semiring Require Import Mat Definitions
-  Listprop Path Semimodule.
-Import ListNotations.
+From HB Require Import structures.
+From Semiring Require Import MatN 
+  SemimoduleN Structures.
+Import ListNotations SemiringNotations.
 
 
 (* ========================================================================= *)
@@ -28,36 +29,16 @@ Section Comp.
 
   Inductive Node := UE | gNB | UPF | DN.
 
-  Definition eqN (x y : Node) : bool :=
-    match x, y with
-    | UE,  UE  => true | gNB, gNB => true
-    | UPF, UPF => true | DN,  DN  => true
-    | _,   _   => false
-    end.
-
   (* Bandwidth: Nat with an Infinity marker *)
   Inductive BW :=
     | BW_fin : nat -> BW
     | BW_inf : BW.
 
-  Definition eqBW (x y : BW) : bool :=
-    match x, y with
-    | BW_fin a, BW_fin b => Nat.eqb a b
-    | BW_inf, BW_inf => true
-    | _, _ => false
-    end.
-
+ 
   (* Product semiring: R = Latency(nat) x Bandwidth(BW) *)
   Inductive R :=
     | Rpair : nat -> BW -> R
     | Unreachable : R.
-
-  Definition eqR (u v : R) : bool :=
-    match u, v with
-    | Rpair l1 b1, Rpair l2 b2 => Nat.eqb l1 l2 && eqBW b1 b2
-    | Unreachable, Unreachable => true
-    | _, _ => false
-    end.
 
   Definition zeroR : R := Unreachable.
   Definition oneR : R := Rpair 0 BW_inf.
@@ -92,373 +73,138 @@ Section Comp.
 
   Definition finN : list Node := [UE; gNB; UPF; DN].
 
-  (* 5G Network Adjacency Matrix *)
-  Definition fiveG_adj (u v : Node) : R :=
-    match u, v with
-    | UE,  UE  => oneR | gNB, gNB => oneR
-    | UPF, UPF => oneR | DN,  DN  => oneR
-    | UE,  gNB => Rpair 1  (BW_fin 1000)
-    | gNB, UE  => Rpair 1  (BW_fin 1000)
-    | gNB, UPF => Rpair 5  (BW_fin 10000)
-    | UPF, gNB => Rpair 5  (BW_fin 10000)
-    | UPF, DN  => Rpair 2  (BW_fin 5000)
-    | DN,  UPF => Rpair 2  (BW_fin 5000)
-    | UE,  UPF => Rpair 10 (BW_fin 100)
-    | UPF, UE  => Rpair 10 (BW_fin 100)
-    | gNB, DN  => Rpair 8  (BW_fin 2000)
-    | DN,  gNB => Rpair 8  (BW_fin 2000)
-    | UE,  DN  => Unreachable
-    | DN,  UE  => Unreachable
-    end.
-
-  Definition fiveG_kleene : Path.Matrix Node R :=
-    matrix_exp_binary_eff_fun Node eqN finN R zeroR oneR plusR mulR fiveG_adj 3%N.
-
 End Comp.
 
 
-Section Proofs.
+(* =================================================================== *)
+(*  HB Instances: FinType Node, BoundedSemiring R                       *)
+(*                                                                       *)
+(*  R = Latency(nat) x Bandwidth(BW) is the product of two bounded      *)
+(*  semirings: min-plus (latency) x max-min (bandwidth).                *)
+(* =================================================================== *)
 
-  Theorem refN : brel_reflexive Node eqN.
-  Proof. unfold brel_reflexive; intros [ | | | ]; simpl; reflexivity. Qed.
+Section HBInstances.
 
-  Theorem symN : brel_symmetric Node eqN.
+  Definition fin_eq_dec (x y : Node) : {x = y} + {x <> y}.
+  Proof. decide equality. Defined.
+
+  Definition elements_list : list Node := [UE; gNB; UPF; DN].
+
+  Lemma elements_nodup_proof : NoDup elements_list.
   Proof.
-    unfold brel_symmetric;
-    intros [ | | | ] [ | | | ]; simpl;
-    try reflexivity; try congruence.
+    unfold elements_list.
+    apply NoDup_cons. intro H. simpl in H. destruct H as [Heq|H]; [inversion Heq|]. simpl in H. destruct H as [Heq|H]; [inversion Heq|]. simpl in H. destruct H as [Heq|H]; [inversion Heq|]. simpl in H. destruct H.
+    apply NoDup_cons. intro H. simpl in H. destruct H as [Heq|H]; [inversion Heq|]. simpl in H. destruct H as [Heq|H]; [inversion Heq|]. simpl in H. destruct H.
+    apply NoDup_cons. intro H. simpl in H. destruct H as [Heq|H]; [inversion Heq|]. simpl in H. destruct H.
+    apply NoDup_cons. intro H. simpl in H. destruct H.
+    apply NoDup_nil.
   Qed.
 
-  Theorem trnN : brel_transitive Node eqN.
+  Lemma elements_complete_proof : forall x : Node, In x elements_list.
+  Proof. unfold elements_list; intros [ | | | ]; simpl; auto. Qed.
+
+  Lemma elements_two_or_more_proof : (2 <= List.length elements_list)%nat.
+  Proof. unfold elements_list. cbn. nia. Qed.
+
+  HB.instance Definition _ := IsFinType.Build Node
+    elements_list elements_nodup_proof elements_complete_proof
+    elements_two_or_more_proof fin_eq_dec.
+
+  Lemma addA_proof : forall x y z : R, plusR (plusR x y) z = plusR x (plusR y z).
   Proof.
-    unfold brel_transitive;
-    intros [ | | | ] [ | | | ] [ | | | ];
-    simpl; intros Ha Hb; try firstorder.
+    intros [l1 b1|] [l2 b2|] [l3 b3|]; cbn; try reflexivity.
+    rewrite PeanoNat.Nat.min_assoc.
+    destruct b1, b2, b3; cbn; try reflexivity.
+    rewrite PeanoNat.Nat.max_assoc. reflexivity.
   Qed.
 
-  Theorem dunN : no_dup Node eqN finN = true.
-  Proof. reflexivity. Qed.
-
-  Theorem lenN : 2 <= List.length finN.
-  Proof. cbn; nia. Qed.
-
-  Theorem memN : forall x : Node, in_list eqN finN x = true.
-  Proof. intros [ | | | ]; cbn; reflexivity. Qed.
-
-  Theorem refR : brel_reflexive R eqR.
+  Lemma addC_proof : forall x y : R, plusR x y = plusR y x.
   Proof.
-    unfold brel_reflexive; intros a.
-    destruct a as [l b | ]; simpl.
-    - rewrite PeanoNat.Nat.eqb_refl.
-      destruct b; simpl; [apply PeanoNat.Nat.eqb_refl | reflexivity].
+    intros [l1 b1|] [l2 b2|]; cbn; try reflexivity.
+    rewrite PeanoNat.Nat.min_comm.
+    destruct b1, b2; cbn; try reflexivity.
+    rewrite PeanoNat.Nat.max_comm. reflexivity.
+  Qed.
+
+  Lemma add0r_proof : forall x : R, plusR zeroR x = x.
+  Proof. intros [l b|]; cbn; reflexivity. Qed.
+
+  Lemma addr0_proof : forall x : R, plusR x zeroR = x.
+  Proof. intros [l b|]; cbn; reflexivity. Qed.
+
+  HB.instance Definition _ := IsCommutativeMonoid.Build R
+    zeroR plusR addA_proof addC_proof add0r_proof addr0_proof.
+
+  Lemma mulA_proof : forall a b c : R, mulR (mulR a b) c = mulR a (mulR b c).
+  Proof.
+    intros [l1 b1|] [l2 b2|] [l3 b3|]; cbn; try reflexivity.
+    rewrite PeanoNat.Nat.add_assoc.
+    destruct b1, b2, b3; cbn; try reflexivity.
+    rewrite PeanoNat.Nat.min_assoc. reflexivity.
+  Qed.
+
+  Lemma mul1r_proof : forall a : R, mulR oneR a = a.
+  Proof.
+    intros [l b|]; cbn.
+    - destruct b; cbn; f_equal; try lia; try reflexivity.
     - reflexivity.
   Qed.
 
-  Theorem symR : brel_symmetric R eqR.
+  Lemma mulr1_proof : forall a : R, mulR a oneR = a.
   Proof.
-    unfold brel_symmetric; intros a b.
-    destruct a as [l1 b1 | ], b as [l2 b2 | ]; cbn;
-    
-    intros Ha; try discriminate; try reflexivity.
-    apply Bool.andb_true_iff in Ha; destruct Ha as [Hl Hb].
-    apply Bool.andb_true_iff; split.
-    - apply PeanoNat.Nat.eqb_eq in Hl; subst; apply PeanoNat.Nat.eqb_refl.
-    - destruct b1, b2; simpl in *; try discriminate; try reflexivity.
-      apply PeanoNat.Nat.eqb_eq in Hb; subst; apply PeanoNat.Nat.eqb_refl.
-  Qed.
-
-  Theorem trnR : brel_transitive R eqR.
-  Proof.
-    unfold brel_transitive; intros a b c.
-    destruct a as [l1 b1 | ], b as [l2 b2 | ], c as [l3 b3 | ];
-    cbn; intros Ha Hb; try discriminate; try reflexivity.
-    apply Bool.andb_true_iff in Ha; destruct Ha as [Hl1 Hb1].
-    apply Bool.andb_true_iff in Hb; destruct Hb as [Hl2 Hb2].
-    apply Bool.andb_true_iff; split.
-    - apply PeanoNat.Nat.eqb_eq in Hl1; apply PeanoNat.Nat.eqb_eq in Hl2;
-      subst; apply PeanoNat.Nat.eqb_refl.
-    - destruct b1, b2, b3; simpl in *; try discriminate; try reflexivity.
-      apply PeanoNat.Nat.eqb_eq in Hb1; apply PeanoNat.Nat.eqb_eq in Hb2;
-      subst; apply PeanoNat.Nat.eqb_refl.
-  Qed.
-
-  Ltac destruct_R :=
-    repeat match goal with
-    | [ a : R |- _ ] => destruct a as [l b | ]
-    end.
-
-  Ltac destruct_BW :=
-    repeat match goal with
-    | [ b : BW |- _ ] => destruct b
-    end.
-
-  Theorem zero_left_identity_plus : forall r : R,
-    eqR (plusR zeroR r) r = true.
-  Proof.
-    intros r; destruct r as [l b | ]; repeat unfold plusR; simpl.
-    - destruct b; simpl; rewrite ?PeanoNat.Nat.eqb_refl; reflexivity.
+    intros [l b|]; cbn.
+    - destruct b; cbn; f_equal; try lia; try reflexivity.
     - reflexivity.
   Qed.
 
-  Theorem zero_right_identity_plus : forall r : R,
-    eqR (plusR r zeroR) r = true.
+  Lemma mulDr_proof : forall a b c : R,
+    mulR (plusR a b) c = plusR (mulR a c) (mulR b c).
   Proof.
-    intros r; destruct r as [l b | ]; repeat unfold plusR; simpl.
-    - destruct b; simpl; rewrite ?PeanoNat.Nat.eqb_refl; reflexivity.
-    - reflexivity.
+    intros [l1 b1|] [l2 b2|] [l3 b3|]; cbn; try reflexivity;
+      f_equal; try nia.
+    (* bandwidth component: max-min distributivity *)
+    destruct b1, b2, b3; cbn; try reflexivity; f_equal; nia.
   Qed.
 
-  Theorem plus_associative : forall a b c : R,
-    eqR (plusR a (plusR b c)) (plusR (plusR a b) c) = true.
+  Lemma mulDl_proof : forall a b c : R,
+    mulR a (plusR b c) = plusR (mulR a b) (mulR a c).
   Proof.
-    intros a b c.
-    destruct a as [la ba|], b as [lb bb|], c as [lc bc|]; unfold plusR.
-    - apply Bool.andb_true_iff; split.
-      + try (apply PeanoNat.Nat.eqb_eq; rewrite PeanoNat.Nat.min_assoc; reflexivity).
-      + destruct ba, bb, bc; simpl; try reflexivity.
-        try (apply PeanoNat.Nat.eqb_eq; rewrite PeanoNat.Nat.max_assoc; reflexivity).
-    - apply refR. - apply refR. - apply refR. - apply refR.
-    - apply refR. - apply refR. - apply refR.
+    intros [l1 b1|] [l2 b2|] [l3 b3|]; cbn; try reflexivity;
+      f_equal; try nia.
+    destruct b1, b2, b3; cbn; try reflexivity; f_equal; nia.
   Qed.
 
-  Theorem plus_commutative : forall a b : R,
-    eqR (plusR a b) (plusR b a) = true.
+  Lemma mul0r_proof : forall a : R, mulR zeroR a = zeroR.
+  Proof. intros [l b|]; cbn; reflexivity. Qed.
+
+  Lemma mulr0_proof : forall a : R, mulR a zeroR = zeroR.
+  Proof. intros [l b|]; cbn; reflexivity. Qed.
+
+  HB.instance Definition _ := IsSemiring.Build R
+    oneR mulR mulA_proof mul1r_proof mulr1_proof
+    mulDr_proof mulDl_proof mul0r_proof mulr0_proof.
+
+  Lemma add_bound_proof : forall a : R, plusR oneR a = oneR.
   Proof.
-    intros a b.
-    destruct a as [la ba|], b as [lb bb|]; unfold plusR.
-    - apply Bool.andb_true_iff; split.
-      + apply PeanoNat.Nat.eqb_eq; nia.
-      + destruct ba, bb; simpl; auto; apply PeanoNat.Nat.eqb_eq; nia.
-    - apply refR. - apply refR. - apply refR.
+    intros [l b|]; cbn; try reflexivity.
   Qed.
 
-  Theorem one_left_identity_mul : forall r : R,
-    eqR (mulR oneR r) r = true.
-  Proof.
-    intros r; destruct r as [l b | ]; lazy [mulR oneR].
-    - destruct b; apply refR.
-    - apply refR.
-  Qed.
+  HB.instance Definition _ := IsBoundedSemiring.Build R add_bound_proof.
 
-  Theorem one_right_identity_mul : forall r : R,
-    eqR (mulR r oneR) r = true.
-  Proof.
-    intros r; destruct r as [l b | ]; lazy [mulR oneR].
-    - rewrite PeanoNat.Nat.add_0_r. destruct b; apply refR.
-    - apply refR.
-  Qed.
+  HB.instance Definition _ := IsSemimodule.Build R R
+    mulR mulDl_proof mulDr_proof
+    (fun a b x => eq_sym (mulA_proof a b x))
+    mul1r_proof mul0r_proof mulr0_proof.
 
-  Theorem mul_associative : forall a b c : R,
-    eqR (mulR a (mulR b c)) (mulR (mulR a b) c) = true.
-  Proof.
-    intros a b c.
-    destruct a as [la ba|], b as [lb bb|], c as [lc bc|]; unfold mulR.
-    - apply Bool.andb_true_iff; split.
-      + apply PeanoNat.Nat.eqb_eq; nia.
-      + destruct ba, bb, bc; simpl; auto; apply PeanoNat.Nat.eqb_eq; nia.
-    - apply refR. - apply refR. - apply refR. - apply refR.
-    - apply refR. - apply refR. - apply refR.
-  Qed.
+End HBInstances.
 
-  Theorem left_distributive_mul_over_plus : forall a b c : R,
-    eqR (mulR a (plusR b c)) (plusR (mulR a b) (mulR a c)) = true.
-  Proof.
-    intros a b c.
-    destruct a as [la ba|], b as [lb bb|], c as [lc bc|]; unfold mulR, plusR.
-    - apply Bool.andb_true_iff; split.
-      + apply PeanoNat.Nat.eqb_eq; nia.
-      + destruct ba, bb, bc; simpl; auto; apply PeanoNat.Nat.eqb_eq; nia.
-    - apply refR. - apply refR. - apply refR. - apply refR.
-    - apply refR. - apply refR. - apply refR.
-  Qed.
+(** Kleene star of the 5G adjacency matrix: [A* = A³].
+    Computes optimal path weights between all node pairs. *)
+Definition fiveG_kleene (m : Node -> Node -> R) : Node -> Node -> R :=
+  powN_fun m 3%N.
 
-  Theorem right_distributive_mul_over_plus : forall a b c : R,
-    eqR (mulR (plusR a b) c) (plusR (mulR a c) (mulR b c)) = true.
-  Proof.
-    intros a b c.
-    destruct a as [la ba|], b as [lb bb|], c as [lc bc|]; unfold mulR, plusR.
-    - apply Bool.andb_true_iff; split.
-      + apply PeanoNat.Nat.eqb_eq; nia.
-      + destruct ba, bb, bc; simpl; auto; apply PeanoNat.Nat.eqb_eq; nia.
-    - apply refR. - apply refR. - apply refR. - apply refR.
-    - apply refR. - apply refR. - apply refR.
-  Qed.
+Definition mva_eff_fun (m : Node -> Node -> R) (v : Node -> R) : Node -> R :=
+  SemimoduleN.matrix_vector_action_eff_fun m v.
 
-  Theorem zero_left_anhilator_mul : forall a : R,
-    eqR (mulR zeroR a) zeroR = true.
-  Proof.
-    intros a; destruct a as [l b | ]; compute; reflexivity.
-  Qed.
-
-  Theorem zero_right_anhilator_mul : forall a : R,
-    eqR (mulR a zeroR) zeroR = true.
-  Proof.
-    intros a; destruct a as [l b | ]; compute; reflexivity.
-  Qed.
-
-  Theorem bounded_one_plus : forall a : R,
-    eqR (plusR oneR a) oneR = true.
-  Proof.
-    intros a; destruct a as [l b | ]; lazy [plusR oneR]; apply refR.
-  Qed.
-
-  Theorem congrP : bop_congruence R eqR plusR.
-  Proof.
-    (* True by case analysis on all four arguments.
-       Since eqR is componentwise equality and plusR is componentwise min/max,
-       congruence follows from congruence of Nat.min and Nat.max. *)
-  Admitted.
-
-  Theorem congrM : bop_congruence R eqR mulR.
-  Proof.
-    (* True by case analysis on all four arguments.
-       Since eqR is componentwise equality and mulR is componentwise add/min,
-       congruence follows from congruence of Nat.add and Nat.min. *)
-  Admitted.
-
-  Theorem congrR : brel_congruence R eqR eqR.
-  Proof.
-    (* True by case analysis on all four arguments.
-       Since eqR is componentwise equality, congruence is immediate. *)
-  Admitted.
-
-  Theorem fiveG_diag_one : forall u v : Node,
-    eqN u v = true -> eqR (fiveG_adj u v) oneR = true.
-  Proof.
-    intros u v Heq.
-    destruct u, v; try discriminate; apply refR.
-  Qed.
-
-  Theorem fiveG_mat_cong : mat_cong Node eqN R eqR fiveG_adj.
-  Proof.
-    unfold mat_cong; intros a b c d Heq_row Heq_col.
-    destruct a, c; simpl in Heq_row; try discriminate;
-    destruct b, d; simpl in Heq_col; try discriminate;
-    apply refR.
-  Qed.
-
-
-  (* =================================================================== *)
-  (*  Instantiate the semimodule:  V := R,  scale := mulR                  *)
-  (*                                                                       *)
-  (*  Since the vector space is the semiring acting on itself via          *)
-  (*  multiplication, all scale axioms reduce to the semiring axioms.      *)
-  (*  This gives us matrix_vector_action_eff_fun and Kleene theorems.      *)
-  (* =================================================================== *)
-
-  (* Vector space V = R, with scale := mulR *)
-  Definition V' := R.
-  Definition zeroV' := zeroR.
-  Definition plusV' := plusR.
-  Definition eqV' := eqR.
-  Definition scale' (a : R) (v : R) : R := mulR a v.
-
-  (* Efficient matrix-vector action from Semimodule *)
-  Definition mva_eff_fun :=
-    Semimodule.matrix_vector_action_eff_fun R V' zeroV' plusV' scale' Node eqN finN.
-
-  Definition mva_func :=
-    Semimodule.matrix_vector_action R V' zeroV' plusV' scale' Node finN.
-
-  (* -------------------------------------------------------------------- *)
-  (*  Example source vector: start at UE with (0, ∞)                       *)
-  (* -------------------------------------------------------------------- *)
-
-  Definition source_vector (n : Node) : V' :=
-    match n with
-    | UE  => oneR       (* (0, ∞): optimal starting state *)
-    | gNB => zeroR      (* Unreachable *)
-    | UPF => zeroR
-    | DN  => zeroR
-    end.
-
-  (* Efficient computation of A · b *)
-  Definition result_eff : Node -> V' :=
-    mva_eff_fun fiveG_adj source_vector.
-
-  (* Functional computation of A · b *)
-  Definition result_func : Node -> V' :=
-    mva_func fiveG_adj source_vector.
-
-  (* -------------------------------------------------------------------- *)
-  (*  Scale axioms: all reduce to the semiring axioms                      *)
-  (* -------------------------------------------------------------------- *)
-
-  Lemma scale_distr_v_sm :
-    forall a x y, eqV' (scale' a (plusV' x y))
-                       (plusV' (scale' a x) (scale' a y)) = true.
-  Proof.
-    intros. unfold scale', plusV', eqV'.
-    apply left_distributive_mul_over_plus.
-  Qed.
-
-  Lemma scale_distr_r_sm :
-    forall a b x, eqV' (scale' (plusR a b) x)
-                       (plusV' (scale' a x) (scale' b x)) = true.
-  Proof.
-    intros. unfold scale', plusV', eqV'.
-    apply right_distributive_mul_over_plus.
-  Qed.
-
-  Lemma scale_assoc_sm :
-    forall a b x, eqV' (scale' a (scale' b x))
-                       (scale' (mulR a b) x) = true.
-  Proof.
-    intros. unfold scale', eqV'.
-    apply mul_associative.
-  Qed.
-
-  Lemma scale_one_sm : forall x, eqV' (scale' oneR x) x = true.
-  Proof.
-    intros. unfold scale', eqV'. apply one_left_identity_mul.
-  Qed.
-
-  Lemma scale_zero_r_sm : forall x, eqV' (scale' zeroR x) zeroV' = true.
-  Proof.
-    intros. unfold scale', zeroV'. apply zero_left_anhilator_mul.
-  Qed.
-
-  Lemma scale_zero_v_sm : forall a, eqV' (scale' a zeroV') zeroV' = true.
-  Proof.
-    intros. unfold scale', zeroV'. apply zero_right_anhilator_mul.
-  Qed.
-
-  Lemma congrS_sm :
-    forall s1 s2 t1 t2,
-      eqR s1 t1 = true -> eqV' s2 t2 = true ->
-      eqV' (scale' s1 s2) (scale' t1 t2) = true.
-  Proof.
-    intros. unfold scale', eqV'. apply (congrM s1 s2 t1 t2 H H0).
-  Qed.
-
-  (* -------------------------------------------------------------------- *)
-  (*  Concrete verification: efficient = functional for our 5G network     *)
-  (* -------------------------------------------------------------------- *)
-
-  (* Since finN = [UE; gNB; UPF; DN] has only 4 nodes, we can compute     *)
-  (* directly.  This demonstrates that matrix_vector_action_eff_fun        *)
-  (* produces the same result as the functional matrix_vector_action.      *)
-
-  Lemma mva_eff_fun_eq_concrete :
-    forall (i : Node),
-      eqR (mva_eff_fun fiveG_adj source_vector i)
-          (mva_func fiveG_adj source_vector i) = true.
-  Proof.
-    unfold mva_eff_fun, mva_func.
-    unfold Semimodule.matrix_vector_action_eff_fun.
-    unfold Semimodule.matrix_vector_action.
-    unfold source_vector.
-    (* For each of the 4 nodes, compute both sides and compare *)
-    destruct i; compute; reflexivity.
-  Qed.
-
-  (* We can also inspect the computed result.  For example, from UE:       *)
-  (*   result_eff UE = plusR (scale (fiveG_adj UE gNB) (source_vector gNB)) *)
-  (*                          (scale (fiveG_adj UE UPF) (source_vector UPF)) *)
-  (*                  = plusR (scale (Rpair 1 (BW_fin 1000)) zeroR)          *)
-  (*                          (scale (Rpair 10 (BW_fin 100)) zeroR)          *)
-  (*                  = plusR zeroR zeroR = zeroR                             *)
-  (* Wait — since source_vector only has UE = oneR, we need to also add    *)
-  (* the UE→UE self-loop:  scale oneR oneR = oneR.  So result = oneR.      *)
-
-End Proofs.
+Definition mva_func (m : Node -> Node -> R) (v : Node -> R) : Node -> R :=
+  SemimoduleN.matrix_vector_action m v.
--- executable/fivegslicing/main.ml
+++ executable/fivegslicing/main.ml
@@ -20,12 +20,26 @@ let string_value : coq_R -> string = function
       "(lat=" ^ string_of_int lat ^ "ms, bw=" ^ string_bw bw ^ ")"
   | Unreachable -> "⊥ (unreachable)"
 
+  
+let rank (n : coq_Node) : int =
+  match n with UE -> 0 | Coq_gNB -> 1 | UPF -> 2 | DN -> 3
+
 (* -------------------------------------------------------------------- *)
-(*  Build the adjacency matrix from the Coq definition                  *)
+(*  5G Network Adjacency Matrix (hardcoded, order: UE, gNB, UPF, DN)    *)
 (* -------------------------------------------------------------------- *)
 
-(* fiveG_adj is extracted as a function Node -> Node -> R *)
-(* We can call it directly *)
+let fiveG_adj_matrix : coq_R array array =
+  [|
+    (* UE *)   [| oneR;                  Rpair (1,  BW_fin 1000);  Rpair (10, BW_fin 100);  Unreachable |];
+    (* gNB *)  [| Rpair (1,  BW_fin 1000);  oneR;                 Rpair (5,  BW_fin 10000); Rpair (8, BW_fin 2000) |];
+    (* UPF *)  [| Rpair (10, BW_fin 100);   Rpair (5,  BW_fin 10000); oneR;                 Rpair (2, BW_fin 5000) |];
+    (* DN *)   [| Unreachable;              Rpair (8,  BW_fin 2000);  Rpair (2,  BW_fin 5000);  oneR |]
+  |]
+
+
+let arraymat (x : coq_Node) (y : coq_Node) : coq_R =
+  fiveG_adj_matrix.(rank x).(rank y)
+
 
 (* -------------------------------------------------------------------- *)
 (*  Print the adjacency matrix (link weights)                           *)
@@ -35,7 +49,7 @@ let print_adjacency () =
   print_endline "\n=== 5G Network Adjacency Matrix ===";
   Stdlib.List.iter (fun u ->
     Stdlib.List.iter (fun v ->
-      let w = fiveG_adj u v in
+      let w = arraymat u v in
       if w <> zeroR then
         Printf.printf "  %-3s → %-3s : %s\n"
           (string_node u) (string_node v) (string_value w)
@@ -48,7 +62,7 @@ let print_adjacency () =
 
 let print_kleene () =
   print_endline "\n=== Kleene Star A* (optimal path weights) ===";
-  let star = fiveG_kleene in
+  let star = fiveG_kleene arraymat in
   Stdlib.List.iter (fun u ->
     Stdlib.List.iter (fun v ->
       let w = star u v in
@@ -59,19 +73,25 @@ let print_kleene () =
   ) finN
 
 (* -------------------------------------------------------------------- *)
-(*  Compute and print A·b (matrix-vector action)                        *)
-(*  b = source vector: UE=(0,∞), others=⊥                              *)
+(*  Source vector: UE = oneR, others = Unreachable                      *)
+(* -------------------------------------------------------------------- *)
+
+let source_vec (n : coq_Node) : coq_R =
+  match n with UE -> oneR | _ -> zeroR
+
+(* -------------------------------------------------------------------- *)
+(*  Matrix-vector action A·b (one-step from source)                     *)
 (* -------------------------------------------------------------------- *)
 
 let print_matrix_vector () =
   print_endline "\n=== A · b  (one-step reachable from UE) ===";
-  let action = result_func in  (* functional version *)
+  let action = mva_func arraymat source_vec in
   Stdlib.List.iter (fun u ->
     Printf.printf "  %-3s : %s\n"
       (string_node u) (string_value (action u))
   ) finN;
   print_endline "\n=== A · b  (efficient list-based) ===";
-  let action_eff = result_eff in
+  let action_eff = mva_eff_fun arraymat source_vec in
   Stdlib.List.iter (fun u ->
     Printf.printf "  %-3s : %s\n"
       (string_node u) (string_value (action_eff u))
@@ -83,15 +103,30 @@ let print_matrix_vector () =
 
 let print_star_vector () =
   print_endline "\n=== A* · b  (optimal paths from UE to all nodes) ===";
-  (* A* · b = vector of optimal weights from source to each node *)
-  (* We compute this using the Kleene star matrix and the source vector *)
-  (* A*·b_i = Σ_j scale (A*_ij) (b_j) *)
-  (* Since b only has UE = oneR, this is A*_iu * oneR = A*_iu for each i *)
-  let star = fiveG_kleene in
+  let star = fiveG_kleene arraymat in
   Stdlib.List.iter (fun u ->
     let w = star u UE in
     Printf.printf "  %-3s : %s\n"
       (string_node u) (string_value w)
+  ) finN
+
+(* -------------------------------------------------------------------- *)
+(*  Verify A*·b via fixed-point iteration (converges in ≤3 steps)       *)
+(* -------------------------------------------------------------------- *)
+
+let print_comparison () =
+  print_endline "\n=== Verification: A*·b vs A³·b (fixed point) ===";
+  let star = fiveG_kleene arraymat in
+  let b = source_vec in
+  let a1b = mva_eff_fun arraymat b in
+  let a2b = mva_eff_fun arraymat a1b in
+  let a3b = mva_eff_fun arraymat a2b in
+  Stdlib.List.iter (fun u ->
+    let star_val = star u UE in
+    let iter_val = a3b u in
+    let tick = if star_val = iter_val then "✓" else "✗" in
+    Printf.printf "  %-3s : %-30s  (A*·b)  vs  %-30s  (A³·b)  %s\n"
+      (string_node u) (string_value star_val) (string_value iter_val) tick
   ) finN
 
 (* -------------------------------------------------------------------- *)
@@ -107,4 +142,4 @@ let () =
   print_matrix_vector ();
   print_kleene ();
   print_star_vector ();
-  print_endline "\nDone."
+  print_comparison ();
